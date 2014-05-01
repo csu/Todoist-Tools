f94a5f7a771f865646e866af42f41d514803434a
Christopher Su <christophersu9@gmail.com>
2014-05-01 00:58:45 -0700

    added example for date, standard @ does not work

--- simple-file-to-tasks.py
+++ simple-file-to-tasks.py
@@ -7,7 +7,7 @@ fname = raw_input('Enter file name: ')
 project_id = raw_input('Project ID: ')
 priority = raw_input('Priority (1 to 4, 4 highest): ')
 indent = raw_input('Indent (1 to 4, 1 top-level): ')
-date_string = raw_input('Date (string): ')
+date_string = raw_input('Date (string) (ex. friday at 11pm): ')
 
 with open(fname) as f:
     content = f.readlines()
