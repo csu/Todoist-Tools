e15ffa33532eb34deb531c8be5c4bca2c5db1c8f
Christopher Su <christophersu9@gmail.com>
2015-10-05 14:58:46 -0700

    looops

--- sort_inbox.py
+++ sort_inbox.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from dateutil import tz
 import requests
 import todoist
-from secrets import TODOIST_TOKEN, INBOX_PROJECT_ID
+from secrets import TODOIST_TOKEN, INBOX_PROJECT_ID, RULES
 
 api = todoist.TodoistAPI(TODOIST_TOKEN)
 
@@ -12,14 +12,20 @@ request_url += '&token=' + TODOIST_TOKEN
 inbox_items = requests.get(request_url).json()
 
 for inbox_item in inbox_items:
-    if inbox_item['content'].startswith('CSE 333'):
-        item = api.items.get_by_id(inbox_item['id'])
-        date_string = inbox_item['date_string']
-        date_string = date_string.split(' ')[1:-2]
-        arw = arrow.get(datetime.strptime('Fri 9 Oct 2015', '%a %d %b %Y'), tz.gettz('US/Pacific'))
-        arw = arw.replace(hour=11, minute=15)
-        date_string = arw.format('MM/DD/YYYY @ HH:mm')
-        # due_date_utc = arw.to('utc').format('YYYY-MM-DDTHH:mm')
-        
-        item.update(date_string=date_string)
-        api.commit()
+    for rule in RULES["due_time_by_prefix"]:
+        prefix = rule[0]
+        hour = rule[1]
+        minute = rule[2]
+        if inbox_item['content'].startswith(prefix):
+            try:
+                date_string = inbox_item['date_string']
+                date_string = date_string.split(' ')[1:-2]
+                arw = arrow.get(datetime.strptime(date_string, '%a %d %b %Y'), tz.gettz('US/Pacific'))
+                arw = arw.replace(hour=hour, minute=minute)
+                date_string = arw.format('MM/DD/YYYY @ HH:mm')
+                
+                item = api.items.get_by_id(inbox_item['id'])
+                item.update(date_string=date_string)
+                api.commit()
+            except:
+                print 'Failed on "%s"' % inbox_item['content']
